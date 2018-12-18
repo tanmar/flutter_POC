1d77bab383f9fb28e38f44730e4c9168ed5aa565
Mariusz Tanski <mariusztanski@gmail.com>
2018-12-18 23:44:53 +0100

    format

--- lib/apps_stats_database.dart
+++ lib/apps_stats_database.dart
@@ -9,12 +9,9 @@ final String columnId = "id";
 final String columnAppName = "app_name";
 final String columnUsedTime = "used_time";
 
-
 class AppsStatsDatabase {
   static final AppsStatsDatabase _appsStatsDatabase =
       AppsStatsDatabase._internal();
-
-
 
   Database _db;
   bool didInit = false;
@@ -23,11 +20,10 @@ class AppsStatsDatabase {
 
   AppsStatsDatabase._internal();
 
-  Future<Database> _getDb() async{
-    if(!didInit) await init();
+  Future<Database> _getDb() async {
+    if (!didInit) await init();
     return _db;
   }
-
 
   Future init() async {
     return await _init();
@@ -39,17 +35,16 @@ class AppsStatsDatabase {
     String path = join(databasesPath, 'apps-db');
     _db = await openDatabase(path, version: 1,
         onCreate: (Database db, int version) async {
-          // We dont need create db here
-        });
+      // We dont need create db here
+    });
 //    String tempPath = databasesPath.path;
   }
 
-  Future<List<AppStats>> getAppStats() async{
+  Future<List<AppStats>> getAppStats() async {
     var db = await _getDb();
     var result = await db.rawQuery('SELECT * FROM $tableApps');
     print("tttt " + result.toString());
-    if(result.length == 0)return null;
+    if (result.length == 0) return null;
     return List<AppStats>();
   }
-
 }
